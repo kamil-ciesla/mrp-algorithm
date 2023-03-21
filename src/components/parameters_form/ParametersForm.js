4db3400c1ccd2bc05db3b07d22411bafaa7ad875
Kamil Cieśla <kamil.ciesla@zendesk.com>
2023-03-21 18:51:12 +0100

    connect input parameters to mrp function

--- src/components/parameters_form/ParametersForm.js
+++ src/components/parameters_form/ParametersForm.js
@@ -1,6 +1,30 @@
 import './ParametersForm.css';
+import mrp from '../../mrp_algorithm/mrpAlgorithm'
+import { useState } from 'react';
 
 function ParametersForm() {
+
+    const [ghpLeadTime, setGhpLeadTime] = useState(0);
+    const onGhpLeadTimeChange = event => {
+        setGhpLeadTime(event.target.value);
+    };
+    const [ghpInStock, setGhpInStock] = useState(0);
+    const onGhpInStockChange = event => {
+        setGhpInStock(event.target.value);
+    };
+    const [mrpLeadTime, setMrpLeadTime] = useState(0);
+    const onMrpLeadTimeChange = event => {
+        setMrpLeadTime(event.target.value);
+    };
+    const [batchSize, setBatchSize] = useState(0);
+    const onBatchSizeChange = event => {
+        setBatchSize(event.target.value);
+    };
+    const [mrpInStock, setMrpInSrock] = useState(0);
+    const onMrpInStockChange = event => {
+        setMrpInSrock(event.target.value);
+    };
+
     return (
         <div className="ParametersForm">
             <div className="">
@@ -8,29 +32,44 @@ function ParametersForm() {
             </div>
             <div className="form-parameter">
                 <label htmlFor="">Czas realizacji</label>
-                <input type="number" name="" id="" />
+                <input type="number" name="ghpLeadTime" id="" 
+                onChange={onGhpLeadTimeChange}
+                value={ghpLeadTime}
+                />
             </div>
             <div className="form-parameter">
                 <label htmlFor="">Na stanie</label>
-                <input type="number" name="" id="" />
+                <input type="number" name="ghpInStock" id="" 
+                onChange={onGhpInStockChange}
+                value={ghpInStock}
+                />
             </div>
             <div className="">
                 <h3>Parametry MRP</h3>
             </div>
             <div className="form-parameter">
                 <label htmlFor="">Czas realizacji</label>
-                <input type="number" name="" id="" />
+                <input type="number" name="mrpLeadTime" id="" 
+                onChange={onMrpLeadTimeChange}
+                value={mrpLeadTime}
+                />
             </div>
             <div className="form-parameter">
                 <label htmlFor="">Wielkość partii</label>
-                <input type="number" name="" id="" />
+                <input type="number" name="batchSize" id="" 
+                onChange={onBatchSizeChange}
+                value={batchSize}
+                />
             </div>
             <div className="form-parameter">
                 <label htmlFor="">Na stanie</label>
-                <input type="number" name="" id="" />
+                <input type="number" name="mrpInStock" id="" 
+                onChange={onMrpInStockChange}
+                value={mrpInStock}
+                />
             </div>
             <div className="">
-                <button type="button">Uruchom algorytm</button>
+                <button type="button" onClick={() => { mrp(ghpLeadTime, ghpInStock, mrpLeadTime, batchSize, mrpInStock) }}>Uruchom algorytm</button>
             </div>
         </div>
     )
--- src/mrp_algorithm/mrpAlgorithm.js
+++ src/mrp_algorithm/mrpAlgorithm.js
@@ -1,3 +1,7 @@
-function mrp(){
-    return null;
+function mrp(ghpLeadTime, ghpInStock, mrpLeadTime, batchSize, mrpInStock){
+    console.log('Starting MRP algorithm...')
+    const params = {ghpLeadTime, ghpInStock, mrpLeadTime, batchSize, mrpInStock}
+    console.log("Algorithm_params: " + Object.values(params))
 }
+
+export default mrp
